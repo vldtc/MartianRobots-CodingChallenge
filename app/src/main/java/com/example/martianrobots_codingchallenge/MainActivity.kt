ec9c429e99dfa023458c7ff0466f07f0bb86c494
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-06-17 01:43:25 +0100

    further UI design and implementation

--- app/src/main/java/com/example/martianrobots_codingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/martianrobots_codingchallenge/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.martianrobots_codingchallenge
 
+import MartianRobotsScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.martianrobots_codingchallenge.ui.MartianRobotsScreen
 import com.example.martianrobots_codingchallenge.ui.theme.MartianRobotsCodingChallengeTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/martianrobots_codingchallenge/ui/MartianRobots.kt
+++ app/src/main/java/com/example/martianrobots_codingchallenge/ui/MartianRobots.kt
@@ -1,189 +1,199 @@
-package com.example.martianrobots_codingchallenge.ui
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.martianrobots_codingchallenge.R
+import com.example.martianrobots_codingchallenge.data.Robot
+import com.example.martianrobots_codingchallenge.data.getRobotColor
 
 @Composable
 fun MartianRobotsScreen() {
+    var gridSize by remember { mutableStateOf("") }
+    val robotX by remember { mutableStateOf("") }
+    val robotY by remember { mutableStateOf("") }
+    val robotOrientation by remember { mutableStateOf("") }
+    val robotInstructions by remember { mutableStateOf("") }
+    val robots = remember { mutableStateListOf<Robot>() }
 
-
-    Column {
-        ChallengeHeader()
-        GridSizeSection()
-        RobotInitialPositionSection()
-        RobotInstructionsSection()
-        RobotOutputsSection()
-    }
-}
-
-@Composable
-fun ChallengeHeader() {
-    Text(
-        text = "Martian Robots",
-        textAlign = TextAlign.Center,
-        style = MaterialTheme.typography.headlineLarge,
-        fontWeight = FontWeight.Bold,
+    LazyColumn(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(16.dp)
-    )
-}
-
-@Composable
-fun GridSizeSection() {
-    var grid by remember { mutableStateOf("") }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        OutlinedTextField(
-            value = grid,
-            onValueChange = { grid = it },
-            label = {
-                Text(
-                    text = "Grid size"
-                )
-            },
-            placeholder = {
-                Text(
-                    text = "Example: 5 3"
-                )
-            },
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .weight(2f)
-        )
-        Button(
-            onClick = {
-                // Handle button click event
-            },
-            modifier = Modifier
-                .padding(8.dp)
-                .weight(1f)
-        ) {
-            Text(text = "Submit!")
+        item{
+            if(gridSize == ""){
+                GridError()
+            }
+            GridSizeInput(gridSize = gridSize) { gridSize = it }
+            RobotInstructionsSection(
+                gridSize = gridSize,
+                robotX = robotX,
+                robotY = robotY,
+                robotOrientation = robotOrientation,
+                robotInstructions = robotInstructions
+            )
+            GridSizeOutput(gridSize = gridSize, robot = robots.lastOrNull())
+            RobotList(robots = robots)
         }
     }
 }
 
 @Composable
-fun RobotInitialPositionSection() {
-    var initialPosition by remember { mutableStateOf("") }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        OutlinedTextField(
-            value = initialPosition,
-            onValueChange = { initialPosition = it },
-            label = {
-                Text(
-                    text = "Initial robot position"
-                )
-            },
-            placeholder = {
-                Text(
-                    text = "Example: 1 1 N"
-                )
-            },
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .weight(2f)
-        )
-        Button(
-            onClick = {
-                // Handle button click event
-            },
-            modifier = Modifier
-                .padding(8.dp)
-                .weight(1f)
-        ) {
-            Text(text = "Submit!")
-        }
-    }
-}
-
-@Composable
-fun RobotInstructionsSection() {
-    var robotInstructions by remember { mutableStateOf("") }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        OutlinedTextField(
-            value = robotInstructions,
-            onValueChange = { robotInstructions = it },
-            label = {
-                Text(
-                    text = "Robot instructions"
-                )
-            },
-            placeholder = {
-                Text(
-                    text = "Example: RFRFRFRF"
-                )
-            },
-            modifier = Modifier
-                .padding(vertical = 8.dp)
-                .weight(2f)
-        )
-        Button(
-            onClick = {
-                // Handle button click event
-            },
-            modifier = Modifier
-                .padding(8.dp)
-                .weight(1f)
-        ) {
-            Text(text = "Submit!")
-        }
-    }
-}
-
-@Composable
-fun RobotOutputsSection() {
-    var finalPosition by remember { mutableStateOf("") }
-
-    Column(
+fun GridError(){
+    Text(
+        text = stringResource(R.string.grid_error),
         modifier = Modifier
             .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        textAlign = TextAlign.Center,
+        color = Color.Red
+    )
+}
+@Composable
+fun GridSizeInput(gridSize: String, onGridSizeChanged: (String) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
     ) {
         OutlinedTextField(
-            value = finalPosition,
-            onValueChange = { finalPosition = it },
-            label = {
-                Text(
-                    text = "Final position and orientation of the robot"
-                )
-            },
-            readOnly = true,
-            modifier = Modifier.padding(vertical = 8.dp)
+            value = gridSize,
+            onValueChange = onGridSizeChanged,
+            label = { Text(stringResource(R.string.grid_size)) },
+            placeholder = { Text(stringResource(R.string.grid_size_ex)) },
+            modifier = Modifier.weight(1f)
         )
     }
 }
+
+@Composable
+fun RobotInstructionsSection(
+    gridSize: String,
+    robotX: String,
+    robotY: String,
+    robotOrientation: String,
+    robotInstructions: String,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            OutlinedTextField(
+                value = robotX,
+                onValueChange = { },
+                label = { Text(stringResource(R.string.pos_x)) },
+                placeholder = { Text(stringResource(R.string.pos_x_ex)) },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            OutlinedTextField(
+                value = robotY,
+                onValueChange = { },
+                label = { Text(stringResource(R.string.pos_y)) },
+                placeholder = { Text(stringResource(R.string.pos_y_ex)) },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            OutlinedTextField(
+                value = robotOrientation,
+                onValueChange = { },
+                label = { Text(stringResource(R.string.robot_orientation), fontSize = 12.sp) },
+                placeholder = { Text(stringResource(R.string.robot_orientation_ex)) },
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row {
+            OutlinedTextField(
+                value = robotInstructions,
+                onValueChange = { },
+                label = { Text(stringResource(R.string.robot_instructions)) },
+                placeholder = { Text(stringResource(R.string.robot_instructions_ex)) },
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Button(
+            onClick = { }
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
+
+@Composable
+fun GridSizeOutput(gridSize: String, robot: Robot?) {
+    val grid = gridSize.split(" ").mapNotNull { it.toIntOrNull() }
+    if (grid.size == 2) {
+        val rows = grid[1] + 1
+        val columns = grid[0] + 1
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+                ){
+            Divider(modifier = Modifier.padding(vertical = 4.dp))
+            repeat(rows) { row ->
+                Row {
+                    repeat(columns) { column ->
+                        Box(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .padding(2.dp)
+                                .background(
+                                    color = if (robot != null && robot.position.x == column && robot.position.y == rows - row - 1) {
+                                        getRobotColor(robot.orientation)
+                                    } else {
+                                        Color.LightGray
+                                    }
+                                )
+                        )
+                    }
+                }
+            }
+            Divider(modifier = Modifier.padding(vertical = 4.dp))
+        }
+    } else {
+        // Handle incorrect grid size input
+        // Display an error message or show a toast
+    }
+}
+
+@Composable
+fun RobotList(robots: List<Robot>) {
+    Column {
+        Text(stringResource(R.string.robots))
+        Spacer(modifier = Modifier.height(8.dp))
+        for (robot in robots) {
+            Card() {
+                Text("Initial Position: (${robot.initialPosition.x}, ${robot.initialPosition.y}), Orientation: ${robot.initialOrientation}")
+                Text("Output Position: (${robot.position.x}, ${robot.position.y}), Orientation: ${robot.orientation}")
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+}
+
