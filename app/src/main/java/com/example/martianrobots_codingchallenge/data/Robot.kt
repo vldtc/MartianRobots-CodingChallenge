d279e13866e606629ffcc837ab8289ddb663d189
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-06-19 10:06:01 +0100

    Mars Grid better UX for Initial and Output position of the robot // Added Comments on MartianRobots.kt

--- app/src/main/java/com/example/martianrobots_codingchallenge/data/Robot.kt
+++ app/src/main/java/com/example/martianrobots_codingchallenge/data/Robot.kt
@@ -1,49 +1,24 @@
 package com.example.martianrobots_codingchallenge.data
 
-data class Position(val x: Int, val y: Int)
+data class Position(
+    val x: Int,
+    val y: Int
+)
 
-enum class Orientation{
-    N, S, E, W
+enum class Orientation {
+    N,
+    S,
+    E,
+    W
 }
 
-data class Robot(var position: Position, var orientation: Orientation, var isLost: Boolean)
+data class Robot(
+    val initialPosition: Position,
+    val initialOrientation: Orientation,
+    var position: Position,
+    var orientation: Orientation,
+    var isLost: Boolean
+)
 
-fun turnLeft(orientation: Orientation): Orientation {
-    return when (orientation) {
-        Orientation.N -> Orientation.W
-        Orientation.S -> Orientation.E
-        Orientation.E -> Orientation.N
-        Orientation.W -> Orientation.S
-    }
-}
+val scents = mutableSetOf<Position>()
 
-fun turnRight(orientation: Orientation): Orientation {
-    return when (orientation) {
-        Orientation.N -> Orientation.E
-        Orientation.S -> Orientation.W
-        Orientation.E -> Orientation.S
-        Orientation.W -> Orientation.N
-    }
-}
-
-fun moveForward(robot: Robot, grid: Position?, scents: MutableSet<Position>) {
-    val newPosition = when (robot.orientation) {
-        Orientation.N -> Position(robot.position.x, robot.position.y + 1)
-        Orientation.S -> Position(robot.position.x, robot.position.y - 1)
-        Orientation.E -> Position(robot.position.x + 1, robot.position.y)
-        Orientation.W -> Position(robot.position.x - 1, robot.position.y)
-    }
-
-    if (isPositionWithinGrid(newPosition, grid)) {
-        robot.position = newPosition
-    } else {
-        if (!scents.contains(robot.position)) {
-            robot.isLost = true
-            scents.add(robot.position)
-        }
-    }
-}
-
-fun isPositionWithinGrid(position: Position, grid: Position?): Boolean {
-    return grid != null && position.x >= 0 && position.x <= grid.x && position.y >= 0 && position.y <= grid.y
-}
